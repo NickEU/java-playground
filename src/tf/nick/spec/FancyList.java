b3fb136b76bf710fe1e0271ff1665fd5fa114a35
Nick <neverate@gmail.com>
2020-05-28 13:08:06 +0300

    spec: findGCD, powerOfTwo, static init block

---
+++ src/tf/nick/spec/FancyList.java
@@ -0,0 +1,19 @@
+package tf.nick.spec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FancyList {
+    public static List<Integer> numbers;
+
+    // Fancy schmancy initialization
+    static {
+        numbers = new ArrayList<>();
+        for (int i = 1; i <= 50; i++) {
+            int rem = i % 10;
+            int number = rem == 0 ? 10 : rem;
+            numbers.add(number);
+        }
+    }
+
+}
