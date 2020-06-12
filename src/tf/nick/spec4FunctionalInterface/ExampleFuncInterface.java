a9f0a196f33a257dc0a1435ba6d9d8406e80e131
Nick <neverate@gmail.com>
2020-06-12 16:16:19 +0300

    spec: upd example func interface

--- src/tf/nick/spec4FunctionalInterface/ExampleFuncInterface.java
+++ src/tf/nick/spec4FunctionalInterface/ExampleFuncInterface.java
@@ -1,6 +1,6 @@
 package tf.nick.spec4FunctionalInterface;
 
-public class Program3 {
+public class ExampleFuncInterface {
   public static void main(String[] args) {
     Functions f = new Functions(5);
     f.printResults();
