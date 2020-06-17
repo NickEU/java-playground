02b2c2654bc5662b89c7afed03d7cabfc4ecbbb5
Nick <neverate@gmail.com>
2020-06-17 10:52:36 +0300

    iCamp: +twoSum using hashSet

---
+++ src/tf/nick/interviewCamp/IC01TwoSum.java
@@ -0,0 +1,40 @@
+package tf.nick.interviewCamp;
+
+// Given an array of integers, find a pair of integers that sums to a number Target.
+// For e.g, if A = [6,3,5,2,1,7]. Target = 4, Result= [3,1]
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class IC01TwoSum {
+  public static void main(String[] args) {
+    {
+      int[] actual = twoSum(new int[] {6, 3, 5, 2, 1, 7}, 4);
+      int[] expected = new int[] {3, 1};
+      System.out.println(Arrays.equals(actual, expected));
+    }
+    {
+      int[] actual = twoSum(new int[] {6, 3, 5, 2, 1, 7}, 15);
+      int[] expected = null;
+      System.out.println(actual == null);
+    }
+    {
+      int[] actual = twoSum(new int[] {6}, 6);
+      int[] expected = null;
+      System.out.println(actual == null);
+    }
+  }
+  // O(n) Time O(n) Space
+  private static int[] twoSum(int[] nums, int targetSum) {
+    HashSet<Integer> numbersSeen = new HashSet<>();
+    for (int num : nums) {
+      int remainder = targetSum - num;
+      if (numbersSeen.contains(remainder)) {
+        return new int[]{remainder, num};
+      } else {
+        numbersSeen.add(num);
+      }
+    }
+    return null;
+  }
+}
