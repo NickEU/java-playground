08b98ce9f3286083bc71648709ec8d5bd123c86f
Nick <neverate@gmail.com>
2020-06-17 11:02:36 +0300

    iCamp: upd twoSum added test cases

--- src/tf/nick/interviewCamp/IC01TwoSum.java
+++ src/tf/nick/interviewCamp/IC01TwoSum.java
@@ -8,25 +8,14 @@ import java.util.HashSet;
 
 public class IC01TwoSum {
   public static void main(String[] args) {
-    {
-      int[] actual = twoSum(new int[] {6, 3, 5, 2, 1, 7}, 4);
-      int[] expected = new int[] {3, 1};
-      System.out.println(Arrays.equals(actual, expected));
-    }
-    {
-      int[] actual = twoSum(new int[] {6, 3, 5, 2, 1, 7}, 15);
-      int[] expected = null;
-      System.out.println(actual == null);
-    }
-    {
-      int[] actual = twoSum(new int[] {6}, 6);
-      int[] expected = null;
-      System.out.println(actual == null);
-    }
+    testTwoSum();
   }
   // O(n) Time O(n) Space
   private static int[] twoSum(int[] nums, int targetSum) {
     HashSet<Integer> numbersSeen = new HashSet<>();
+    if (nums == null) {
+      return null;
+    }
     for (int num : nums) {
       int remainder = targetSum - num;
       if (numbersSeen.contains(remainder)) {
@@ -36,5 +25,33 @@ public class IC01TwoSum {
       }
     }
     return null;
+  }
+
+  private static void testTwoSum() {
+    {
+      int[] actual = twoSum(new int[] {6, 3, 5, 2, 1, 7}, 4);
+      int[] expected = new int[] {3, 1};
+      System.out.println(Arrays.equals(actual, expected));
+    }
+    {
+      int[] actual = twoSum(new int[] {6, 3, 5, 2, 1, 7}, 15);
+      int[] expected = null;
+      System.out.println(actual == expected);
+    }
+    {
+      int[] actual = twoSum(new int[] {6}, 6);
+      int[] expected = null;
+      System.out.println(actual == expected);
+    }
+    {
+      int[] actual = twoSum(new int[] {}, 15);
+      int[] expected = null;
+      System.out.println(actual == expected);
+    }
+    {
+      int[] actual = twoSum(null, 15);
+      int[] expected = null;
+      System.out.println(actual == expected);
+    }
   }
 }
