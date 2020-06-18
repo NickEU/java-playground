7d204b49630b7820a3c74422ad7990eee4d8b06b
Nick <neverate@gmail.com>
2020-06-18 09:44:59 +0300

    iCamp: 02-matrixIslands: refactored test suite

--- src/tf/nick/interviewCamp/ic02MatrixIslands/CounterTest.java
+++ src/tf/nick/interviewCamp/ic02MatrixIslands/CounterTest.java
@@ -1,24 +1,56 @@
 package tf.nick.interviewCamp.ic02MatrixIslands;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class CounterTest {
-  private static final char[][] testMap = new char[][]
-          {{'1', '0', '1'},
-           {'0', '1', '1'},
-           {'0', '1', '1'}};
+  private static final HashMap<char[][], Integer> testCases = new HashMap<>();
 
-  private static final char[][] testMap2 = new char[][]
-          {{'1', '1', '0'},
-           {'0', '0', '1'},
-           {'1', '1', '0'}};
-
-  private static final char[][] testMap3 =
-          {{'1','0','1','1','1'},
-           {'1','0','1','0','1'},
-           {'1','1','1','0','1'}};
+  static {
+    testCases.put(
+        new char[][] {
+          {'1', '0', '1'},
+          {'0', '1', '1'},
+          {'0', '1', '1'}
+        },
+        2);
+    testCases.put(
+        new char[][] {
+          {'1', '1', '0'},
+          {'0', '0', '1'},
+          {'1', '1', '0'}
+        },
+        3);
+    testCases.put(
+        new char[][] {
+          {'1', '0', '1', '1', '1'},
+          {'1', '0', '1', '0', '1'},
+          {'1', '1', '1', '0', '1'}
+        },
+        1);
+    testCases.put(new char[][] {{'0'}}, 0);
+    testCases.put(new char[][] {{'1'}}, 1);
+  }
 
   public static void runTestCases(ICounter counter) {
-    System.out.println(counter.countIslands(testMap) == 2);
-    System.out.println(counter.countIslands(testMap2) == 3);
-    System.out.println(counter.countIslands(testMap3) == 1);
+    AtomicBoolean allTestsOK = new AtomicBoolean(true);
+    testCases.forEach(
+        (input, expectedResult) -> {
+          int actualResult = counter.countIslands(input);
+          boolean testHasFailed = actualResult != expectedResult;
+          if (testHasFailed) {
+            allTestsOK.set(false);
+            System.out.println("Test has failed. Input:");
+            for (char[] row : input) {
+              System.out.println(Arrays.toString(row));
+            }
+            System.out.println("Expected result: " + expectedResult);
+            System.out.println("Actual result: " + actualResult);
+          }
+        });
+    if (allTestsOK.get()) {
+      System.out.println("All tests have passed!");
+    }
   }
 }
