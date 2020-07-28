08772817e0e2160344649c9ffccc54e2903dd4bd
Nick <neverate@gmail.com>
2020-07-28 15:53:02 +0300

    hyperskill: collections: upd collectingAndThen

--- src/tf/nick/hypertrash/collections/DequeReverse.java
+++ src/tf/nick/hypertrash/collections/DequeReverse.java
@@ -7,19 +7,23 @@ import java.util.stream.Collectors;
 
 public class DequeReverse {
     public static void main(String[] args) {
-        //bad();
-        better();
+        var sc = new Scanner("3 \n" +
+                "1 \n" +
+                "2 \n" +
+                "3 \n");
+        //bad(sc);
+        better(sc);
     }
 
-    private static void better() {
-        new Scanner(System.in).tokens().skip(1)
+    private static void better(Scanner sc) {
+        sc.tokens().skip(1)
                 .collect(Collectors.toCollection(ArrayDeque::new))
                 .descendingIterator()
                 .forEachRemaining(System.out::println);
     }
 
-    private static void bad() {
-        new Scanner(System.in).tokens().skip(1)
+    private static void bad(Scanner sc) {
+        sc.tokens().skip(1)
                 .collect(Collectors.collectingAndThen(Collectors.toList(), l -> {
                     Collections.reverse(l);
                     return l.stream();
