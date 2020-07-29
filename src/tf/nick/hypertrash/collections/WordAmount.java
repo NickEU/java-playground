a8344cc663beca82b9ba7fbf338e3944fbb24936
Nick <neverate@gmail.com>
2020-07-29 15:12:44 +0300

    hyperskill: collections: +WordAmount(War and Peace ex)

---
+++ src/tf/nick/hypertrash/collections/WordAmount.java
@@ -0,0 +1,19 @@
+package tf.nick.hypertrash.collections;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WordAmount {
+    public static void main(String[] args) {
+        Arrays.stream(new Scanner(System.in).nextLine().split("\\s+"))
+                .map(s -> Map.of(s.toLowerCase(), 1))
+                .reduce(new HashMap<>(), (acc, cur) -> {
+                    acc.merge(cur.keySet().iterator().next(),
+                            cur.values().iterator().next(), Integer::sum);
+                    return acc;
+                })
+                .forEach((k, v) -> System.out.println(k + " " + v));
+    }
+}
